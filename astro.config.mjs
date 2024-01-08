318884a1fd90c18598ac20d846fdd5a2dcf4ec19
Saptam <saptampro2003@gmail.com>
2024-01-08 19:14:12 +0530

    Fixed styling in the production pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 
-import vercel from "@astrojs/vercel/serverless";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs()],
   output: "server",
-  adapter: vercel()
+  adapter: cloudflare()
 });
