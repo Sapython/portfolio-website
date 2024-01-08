7fc9724d5e3abb926e1b9bb2ad3c215ca69762f0
Saptam <saptampro2003@gmail.com>
2024-01-08 20:35:44 +0530

    Fixed asset loading.

--- src/components/portfolio/AboutMe/AboutMeDescription.jsx
+++ src/components/portfolio/AboutMe/AboutMeDescription.jsx
@@ -1,5 +1,5 @@
 import { createSignal } from "solid-js";
-import './aboutMeDescription.scss'
+import "./aboutMeDescription.scss";
 export function AboutMeDescription() {
     const [descriptionType,setDescriptionType]  = createSignal("professional")
     const toggle = () => {
